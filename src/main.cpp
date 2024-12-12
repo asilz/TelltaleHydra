e4de4a8cae603006d1148ebe7410fb324a6d31df
Asil Zogby <asilz@stud.ntnu.no>
2024-12-12 15:24:21 +0100

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,19 @@
-int main(void) { return 0; }
+#include <tth/core/any.hpp>
+#include <tth/core/log.hpp>
+#include <tth/meta/container/dcarray.hpp>
+#include <tth/meta/container/map.hpp>
+#include <tth/meta/container/sarray.hpp>
+#include <tth/meta/container/set.hpp>
+#include <tth/meta/meta.hpp>
+
+int main(void)
+{
+    TTH_LOG_INFO("hello\n");
+
+    Any any = Any(2);
+
+    float *a = any.GetTypePtr<float>();
+    int *b = any.GetTypePtr<int>();
+
+    return 0;
+}
