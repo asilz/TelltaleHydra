5569f03bbe02ac8ba41af8f344c9b59388d2cd3f
Asil Zogby <asilz@stud.ntnu.no>
2024-12-21 14:30:41 +0100

    Fix windows error

--- src/core/any.cpp
+++ src/core/any.cpp
@@ -23,18 +23,20 @@
 namespace TTH
 {
 
-typedef int64_t int64;
-typedef int32_t int32;
-typedef int16_t int16;
-typedef int8_t int8;
+using int64 = int64_t;
+using int32 = int32_t;
+using int16 = int16_t;
+using int8 = int8_t;
 
-typedef uint64_t uint64;
-typedef uint32_t uint32;
-typedef uint16_t uint16;
-typedef uint8_t uint8;
+using uint64 = uint64_t;
+using uint32 = uint32_t;
+using uint16 = uint16_t;
+using uint8 = uint8_t;
 
 using unsigned__int64 = uint64_t;
+#ifndef defined(__WIN32__)
 using __int64 = int64_t;
+#endif
 
 Any::Any(uint64_t hash) : obj_(nullptr), dtor_(nullptr), copy_(nullptr), read_(nullptr), write_(nullptr), typeName_(nullptr)
 {
