f9a3a33fda79c6d10c0ec755df0cf4f7f139942a
Asil Zogby <asilz@stud.ntnu.no>
2025-01-24 15:35:07 +0100

    Add Vector2 and Vector4

--- src/linalg/vector.cpp
+++ src/linalg/vector.cpp
@@ -2,6 +2,23 @@
 
 namespace TTH
 {
+int32_t Vector4::Read(Stream &stream)
+{
+    int32_t size = stream.Read(x);
+    size += stream.Read(y);
+    size += stream.Read(z);
+    size += stream.Read(w);
+    return size;
+}
+
+int32_t Vector4::Write(Stream &stream) const
+{
+    int32_t size = stream.Write(x);
+    size += stream.Write(y);
+    size += stream.Write(z);
+    size += stream.Write(w);
+    return size;
+}
 int32_t Vector3::Read(Stream &stream)
 {
     int32_t size = stream.Read(x);
@@ -15,6 +32,20 @@ int32_t Vector3::Write(Stream &stream) const
     int32_t size = stream.Write(x);
     size += stream.Write(y);
     size += stream.Write(z);
+    return size;
+}
+
+int32_t Vector2::Read(Stream &stream)
+{
+    int32_t size = stream.Read(x);
+    size += stream.Read(y);
+    return size;
+}
+
+int32_t Vector2::Write(Stream &stream) const
+{
+    int32_t size = stream.Write(x);
+    size += stream.Write(y);
     return size;
 }
 }; // namespace TTH
